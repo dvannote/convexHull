db5771f05785533a43f8b2b217e86b63a683ef82
Devon S. VanNote <dvon5150@yahoo.com>
2016-10-19 19:53:13 -0500

    fixed do-while - just made it a while loop instead

--- convexHullVanNote/convexHullVanNote.cpp
+++ convexHullVanNote/convexHullVanNote.cpp
@@ -1,6 +1,7 @@
 // Devon VanNote
 // Analysis of Algorithms
-// Program: Convex Hull -Jarvis's March algorithm method
+// Program: Convex Hull 
+// Due: Oct-20-2016
 
 #include <iostream>
 #include <algorithm>
@@ -20,11 +21,11 @@ int main() {
 	int x = 0, y = 0, i = 0;
 	char choice;
 	
-	cout << "Welcome to the Convex Hull Calculator" << endl;
-	cout << "Press e to enter in up to 10 coordinates." << endl;
+	cout << "Welcome to the Convex Hull Calculator (For Right-Handed Coordinate System)" << endl;
+	cout << "Press any character to enter in up to 50 coordinates (positive integer values)." << endl;
 	cin >> choice;
 
-	while (choice != 'q' && i < 10) {
+	while (choice != 'q' && i < 50 && x >= 0 && y >= 0) {
 		cout << "X: ";
 		cin >> x;
 		cout << "Y: ";
@@ -32,12 +33,12 @@ int main() {
 		p[i].x = x;
 		p[i].y = y;
 		i++;
-		cout << endl << "Press e to continue or q to quit" << endl;
+		cout << endl << "Press any character to continue or q to quit" << endl;
 		cin >> choice;
 	}
-	if (i < 3) {
+	if (i < 3 || x < 0 || y < 0) {
 		//invalid input
-		cout << "Any number of points less than three only forms one line segment. Cannot compute Convex Hull." << endl;
+		cout << "Invalid input (negative coordinate || less than 3 coordinates). Cannot compute Convex Hull." << endl;
 	}
 	else {
 		//perform convexHull
@@ -49,35 +50,39 @@ int main() {
 
 void convexHull(point p[], int n) {
 	int leftMost = 0, i = 0, j = 0, k =0;
+	bool firstRun = true;
 	vector<point> result;
 	
 	//get left-most point (lowest x value)
-	for(i = 0; i < n-1; i++) {
-		if (p[i].x <= p[i + 1].x) {
+	for(i = 0; i < n; i++) {
+		if (p[i].x <= p[leftMost].x) {
 			leftMost = i;
 		}
 	}
+
+	cout << "Left-most point is: " << p[leftMost].x << " , " << p[leftMost].y << endl;
+	result.push_back(p[leftMost]); //pushing leftmost onto vector
 
 	//Starting with leftMost point, start calculating cross-product
 	//to determine if points lie on other side of line of next chosen point.
 	//If all points are on other side, add point to result vector.
 
-	j = leftMost;
-	do {
-		result.push_back(p[j]); //adding new point to vector (in first case, its the left-most point)
-
-		k = (j + 1) % n; //remainder of points to check
-
+	while (j != leftMost) {
+		k = (j + 1) % n; //next point to check segment with
+		if (firstRun == true) {
+			j = leftMost;
+			firstRun = false;
+		}
 		for (i = 0; i < n; i++) {
 			if (crossProduct(p[j].x, p[j].y, p[i].x, p[i].y, p[k].x, p[k].y) == 2) {
 				k = i; //next point that satisfies cross product
 			}
-		}
+		} //sets j to be next point checked on run though of do-while loop
+		cout << "Point added to Convex Hull: " << p[k].x << " , " << p[k].y << endl;
+		j = k;
+		result.push_back(p[j]); //adding new point to vector
+	}
 
-		j = k; //sets j to be next point checked on run though of do-while loop
-
-	} while (j != leftMost); 
-	
 	//print result
 	cout << endl << "Convex Hull Result: " << endl;
 	for (int i = 0; i < result.size(); i++) {
